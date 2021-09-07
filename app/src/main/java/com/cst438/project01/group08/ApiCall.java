6fa6c6261b8f8a3b3815ca688683d0f2242238f8
Connor Turetzky <cturetzky@gmail.com>
2021-09-07 10:20:49 -0700

    Added methods and url for api

---
+++ app/src/main/java/com/cst438/project01/group08/ApiCall.java
@@ -0,0 +1,32 @@
+package com.cst438.project01.group08;
+
+public class ApiCall {
+    // URL: https://rapidapi.com/justin-WFnsXH_t6/api/exercisedb
+    // API URL endpoint: https://exercisedb.p.rapidapi.com/exercises
+    // x-rapidapi-key: c91b63fe30mshb3a3bbc701b73f4p164cf9jsn79fbf7d522a7
+    // x-rapidapi-host: exercisedb.p.rapidapi.com
+    /* Example response:
+        {
+    "bodyPart":"string"
+    "equipment":"string"
+    "gifUrl":"string"
+    "id":"string"
+    "name":"string"
+    "target":"string"
+    }
+     */
+    public String getByName(String name){
+
+        return "Placeholder";
+    }
+    public String getByBodypart(String bodypart){
+        return "Placeholder";
+
+    }
+    public String getExercises(){
+
+        return "Placeholder";
+    }
+}
+
+
