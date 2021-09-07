9dbea07807a2a956e8e1145bcbc902f3eb25c829
Angel Gutierrez <anggutierrez@csumb.edu>
2021-09-07 10:53:01 -0700

    Create exercise model

---
+++ app/src/main/java/com/cst438/project01/group08/Exercise.java
@@ -0,0 +1,35 @@
+package com.cst438.project01.group08;
+
+public class Exercise {
+    private String bodyPart;
+    private String equipment;
+    private String gifUrl;
+    private String id;
+    private String name;
+    private String target;
+
+    public String getBodyPart() {
+        return bodyPart;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public String getGifUrl() {
+        return gifUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+}
--- app/src/main/java/com/cst438/project01/group08/Search.java
+++ app/src/main/java/com/cst438/project01/group08/Search.java
@@ -1,7 +1,20 @@
 package com.cst438.project01.group08;
 
 public class Search {
-    public void SearchForExercise() {
+
+    public void searchForExercise(String query) {
+
 
     }
+
+    public void searchByMuscleGroup(String query) {
+
+
+    }
+
+    public void searchByExerciseType(String Query) {
+
+
+    }
+
 }
