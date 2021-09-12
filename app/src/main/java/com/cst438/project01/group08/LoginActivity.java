fa1cf959e71e26c0c29b68fd628b3615fca648a0
ctm93 <al0ve93@aol.com>
2021-09-12 02:51:52 -0700

    Login complete. Registration and logout still in process.

--- app/src/main/java/com/cst438/project01/group08/LoginActivity.java
+++ app/src/main/java/com/cst438/project01/group08/LoginActivity.java
@@ -7,9 +7,22 @@ import android.os.Bundle;
 
 import com.cst438.project01.group08.data.UserDAO;
 import com.cst438.project01.group08.data.UserDataBase;
+import com.cst438.project01.group08.model.User;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText editTextUser, editTextPassword;
+    Button buttonLogin;
+    TextView textViewRegister;
     UserDAO db;
     UserDataBase dataBase;
 
@@ -18,11 +31,43 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        dataBase = Room.databaseBuilder(this, UserDataBase.class, "User")
+        editTextUser = findViewById(R.id.editTextTextPersonName);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+        buttonLogin = findViewById(R.id.button);
+
+        textViewRegister = findViewById(R.id.textViewRegister);
+
+        dataBase = Room.databaseBuilder(this, UserDataBase.class, "mi-database.db")
                 .allowMainThreadQueries()
                 .build();
 
         db = dataBase.getUserDAO();
+
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = editTextUser.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                User user = db.getUser(userName, password);
+                if (user != null) {
+                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                    i.putExtra("User", user);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Unregistered user, or incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
